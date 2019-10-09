e7c474a140bcc5ec0b346a8b951f0e53119dd2c1
Sachin Ruhil <sachinruhil@Sachins-MBP.home>
2019-10-09 01:32:36 -0400

    merge intervals

---
+++ merge_intervals.py
@@ -0,0 +1,20 @@
+# Input: [[1,3],[2,6],[8,10],[15,18]]
+# Output: [[1,6],[8,10],[15,18]]
+# Explanation: Since intervals [1,3] and [2,6] overlaps, merge them into [1,6].
+
+
+def merge_intervals(intervals):
+
+    intervals.sort(key = lambda x:x[0])
+        i = 1
+        while i < len(intervals):
+            if intervals[i][0] <= intervals[i-1][1]:
+                intervals[i-1][0] = min(intervals[i-1][0], intervals[i][0])
+                intervals[i-1][1] = max(intervals[i-1][1], intervals[i][1])
+                
+                intervals.pop(i)
+            else:
+                i += 1
+        return intervals
+
+print(merge_intervals([[1,3],[2,6],[8,10],[15,18]]))
